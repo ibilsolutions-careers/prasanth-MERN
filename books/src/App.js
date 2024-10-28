4c40c4e4d4c3432785674e5b35b633e7f5e91cdb
drwopadi_2002 <drowpadi.rajamohanan@ibilglobal.com>
2024-10-28 15:25:08 +0530

    initia

---
+++ books/src/App.js
@@ -0,0 +1,12 @@
+
+import Booksgallery from "../components/Booksgallery";
+
+function App() {
+  return (
+    <div className="App">
+      <Booksgallery/>
+    </div>
+  );
+}
+
+export default App;
